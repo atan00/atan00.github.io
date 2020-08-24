7de0222baa8068271c5959af7152680003ee0fdb
Autumn Tan <atan@Autumns-MacBook-Pro.local>
2020-08-24 12:19:50 -0500

    Updates

---
+++ precache-manifest.db264fe21e0f8e6c2bc9d576a52bd52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77512d41a526d8344628e33633956d1f",
+    "url": "/atan.github.io/index.html"
+  },
+  {
+    "revision": "761f89e96bfc97ebea1b",
+    "url": "/atan.github.io/static/css/main.46e584d9.chunk.css"
+  },
+  {
+    "revision": "f2c9948d9f70a700a1e6",
+    "url": "/atan.github.io/static/js/2.5c32f41d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/atan.github.io/static/js/2.5c32f41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "761f89e96bfc97ebea1b",
+    "url": "/atan.github.io/static/js/main.20f85c77.chunk.js"
+  },
+  {
+    "revision": "8cd1ab3e1818ab1a901f",
+    "url": "/atan.github.io/static/js/runtime-main.49f5492c.js"
+  }
+]);
