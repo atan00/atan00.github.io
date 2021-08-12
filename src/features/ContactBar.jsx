46b8598ea3cce6d24c86e6647fb7b49215d250aa
atan001 <autumn.tan.1@gmail.com>
2021-08-11 22:54:45 -0400

    minor issues

--- src/features/ContactBar.jsx
+++ src/features/ContactBar.jsx
@@ -13,7 +13,7 @@ class ContactBar extends React.Component {
           <Icon icon="inbox" iconSize={Icon.MAX_SIZE} className={styles.contactIcon}/>
           <span className={styles.email}> aytan@princeton.edu </span>
         </a>
-        <a href="https://linkedin.com/in/autumntan" target="_blank" className={styles.line}>
+        <a href="https://linkedin.com/in/autumntan" target="_blank" rel="noopener noreferrer" className={styles.line}>
           <img src={linkedIn} alt="LinkedIn logo" width="20px" height="17px" className={styles.contactIcon} />
           <span className={styles.email}> autumntan </span>
         </a>
